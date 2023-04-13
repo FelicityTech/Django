612af1a02fefde2cf97ca9eceff9d08b921a7211
Solomon Eniola Adegoke <Souceking@gmail.com>
2023-04-14 00:30:11 +0100

    model

---
+++ Intro_Django/Model/modeling/modelapp/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+class Persona(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+
+class Runner(models.Model):
+    MedalType = models.TextChoices('MedalType', 'GOLD SILVER BRONZE')
+    name = models.CharField(max_length=60)
+    medal = models.CharField(blank=True, choices=MedalType.choices, max_length=10)
+
+
+class Fruit(models.Model):
+
+    name = models.CharField(max_length=100, primary_key=True)
+
+
+
+# Many to many field
+
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __str__(self):
+        return self.name
+    
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
